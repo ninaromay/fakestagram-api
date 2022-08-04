98286d7e286b20dc828eaa8111c3c922c86d174a
nina romay dev <nina.romay@gmail.com>
2022-08-04 11:23:35 +0200

    fluff

--- controller.js
+++ controller.js
@@ -4,7 +4,6 @@ const { User }      = require('./SchemaJs/users')
 
 const getAll = async (req, res) => {
     let data = await Data.find()
-    console.log(data);
     let suggested = await Suggested.find()
     let users = await User.find()
 
@@ -12,12 +11,49 @@ const getAll = async (req, res) => {
     let status = condition ? 200 : 204
     let error = condition ? 'All ok' : 'Not found'
 
-    console.log( data )
     let [objeto] = data 
     let response = {data : objeto, suggested, users, status, error}
     res.status(200).json(response)
 }
 
+const getTagged = async (req, res) => {
+    let data = await User.find()
+
+    let {tag} = req.params
+    let tagged_posts = []
+    data.map((obj)=>{
+        // console.log(obj.posts);
+        // obj.posts.map((post)=>{
+        //     let tag = post.tags.length > 0
+        //     if(tag){
+        //       let tagged = post.filter( tags => obj.user.url === tags.tag )
+        //       tagged_posts.push(tagged);
+        //       console.log(tagged_posts);
+        //     }
+        // })
+        
+        let tagged = obj.posts.filter(post => {
+            if(post.tags.length !== 0) {
+                return post.tags.map(tug => {
+                    if(tag === tug){
+                        tagged_posts.push(post)
+                    }
+                })
+            }
+        })
+
+    })
+
+    console.log(tagged_posts);
+
+    let condition = data.length > 0
+    let status = condition ? 200 : 204
+    let error = condition ? 'All ok' : 'Not found'
+
+    let response = {tagged_posts, status, error}
+    res.status(200).json(response)
+}
+
 module.exports = {
-    getAll
+    getAll, getTagged
 }
--- router.js
+++ router.js
@@ -1,9 +1,14 @@
-const {getAll} = require('./controller')
+const {getAll, getTagged} = require('./controller')
 
 const express = require('express')
 const router = express.Router()
 
 router.route('/')
 .get(getAll)
+
+router.route('/profile/:tag')
+.get(getTagged)
+
+
 
 module.exports = {router}
